4e63fa8e6d778edbd03ada5001f753f6a3230a42
Aldrin Navarro <aldrinnavarro16@gmail.com>
2018-06-02 11:31:50 +0800

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img src="./assets/logo.png"> -->
+    <div class="header">
+      <div class="header-content">
+        <div class="title">
+          <h1>JUNE</h1>
+          <span>Alzheimer's & Brain Awareness Month</span>
+        </div>
+        <div class="content">
+          Go purple and raise awareness this June.
+          <a href="https://twitter.com/alzassociation" target="_blank"><i class="fab fa-twitter"></i></a>
+        </div>
+      </div>
+    </div>
+    <Frame/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Frame from './components/Frame.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Frame
   }
 }
 </script>
@@ -23,6 +35,52 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.header {
+  height: 40vh;
+  background-color: #4a0d66;
+  color: #fff;
+}
+
+.header-content {
+  padding-top: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.header-content h1 {
+  font-size: 40px;
+  font-weight: bold;
+}
+
+.header-content .title {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.header-content .title > h1 {
+  margin-right: 20px;
+  margin-left: 40px;
+}
+
+.header-content .content {
+  padding-top: 20px;
+  text-align: left;
+}
+
+.header-content .content a {
+  color: #f0d5f5;
+}
+
+.header-content .content a .fa-twitter {
+  transition: all 0.2s ease-in;
+}
+.header-content .content a:hover >
+.fa-twitter {
+  color: #fff;
+}
+
 </style>
---
+++ src/components/Frame.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="container effect7">
+    <canvas id="canvas"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+
+  mounted() {
+
+    document.addEventListener('DOMContentLoaded', function() {
+      let canvas = document.getElementById('canvas');
+      canvas.width  = canvas.parentElement.clientWidth;
+      canvas.height = canvas.parentElement.clientHeight;
+
+      let layer1 = {
+        top: canvas.height - 100,
+        height: 100,
+        width: canvas.width
+      };
+
+      if (canvas.getContext) {
+        let ctx = canvas.getContext('2d');
+
+        ctx.fillStyle = '#4a0d66';
+        ctx.fillRect(0, layer1.top, layer1.width, layer1.height);
+
+        let fontSize = 180;
+        ctx.font = `bold ${fontSize}px Arial`;
+        ctx.fillStyle = '#5b236f';
+        ctx.fillText("#", 80, 485);
+
+
+        fontSize = 30;
+        ctx.font = `bold ${fontSize}px Arial`;
+        ctx.fillStyle = '#FFFFFF';
+        ctx.fillText("#ENDALZ", layer1.width/2 - fontSize * 2, layer1.top + layer1.height/2);
+      }
+    });
+
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.container {
+  width: 480px;
+  height: 480px;
+  background: #fff;
+  position: relative;
+  z-index: 2;
+  margin-top: -120px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+
+/* Effects */
+.effect7
+{
+    -webkit-box-shadow:0 4px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset;
+       -moz-box-shadow:0 4px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset;
+            box-shadow:0 4px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset;
+}
+.effect7:after
+{
+    right:10px;
+    left:auto;
+    -webkit-transform:skew(8deg) rotate(3deg);
+       -moz-transform:skew(8deg) rotate(3deg);
+        -ms-transform:skew(8deg) rotate(3deg);
+         -o-transform:skew(8deg) rotate(3deg);
+            transform:skew(8deg) rotate(3deg);
+}
+
+#canvas {
+  width: 100%;
+  height: 100%;
+}
+
+</style>
